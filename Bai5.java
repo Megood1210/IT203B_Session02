9da0bfcfd680241a05c9d563c2fb48ed1ade147f
Megood1210 <xuanvinhle06@gmail.com>
2026-03-10 09:55:13 +0700

    Upload bài tập 5

---
+++ Bai5.java
@@ -0,0 +1,26 @@
+interface UserActions {
+    default void logActivity(String activity) {
+        System.out.println("User log: " + activity);
+    }
+}
+
+interface AdminActions {
+    default void logActivity(String activity) {
+        System.out.println("Admin log: " + activity);
+    }
+}
+
+class SuperAdmin implements UserActions, AdminActions {
+    @Override
+    public void logActivity(String activity) {
+        System.out.println("SuperAdmin log: " + activity);
+    }
+}
+
+public class Bai5 {
+    public static void main(String[] args) {
+        SuperAdmin superadmin = new SuperAdmin();
+
+        superadmin.logActivity("Delete user");
+    }
+}
